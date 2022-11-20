a152dfa3b48bd2b0dc700cdc458476d10d63a714
Josue David peñate ruiz <penateruizjosuedavid@gmail.com>
2022-11-20 11:20:45 -0500

    jsII finalizado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -17,9 +17,9 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
   if (edad >= 18){
-    return  'allowed';
+    return "Allowed";
 }
-  return 'not allowed';
+  return "Not allowed";
 }
   
 function conection(status) {
@@ -29,13 +29,13 @@ function conection(status) {
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
    if (status === 1){
-     return 'online';
+     return "Online";
    }
     else if (status === 2){
-      return 'away';
+      return "Away";
     }
      else {
-       return 'offline';
+       return "Offline";
      }
 }
 
@@ -47,7 +47,7 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
    if (idioma === 'aleman') {
-     return 'guten tag!';
+     return "Guten Tag!";
    }
    if (idioma === 'mandarin') {
     return "Ni Hao!";
@@ -85,8 +85,13 @@ function colors(color) {
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
-  // Tu código:
-  numero === 10 ||  numero === 5;
+  // Tu código:          
+  if (numero === 10 || numero === 5) {
+  return true;               
+   }                                 
+  else {
+  return false; 
+}                                 
 }
 
 function estaEnRango(numero) {
@@ -125,6 +130,21 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+    if (num1 < 0 || num2 < 0 || num3 < 0){ 
+      return "Hay negativos"
+    }
+  else if (num1 > 0 && num1 > num2 && num1 > num3){
+    return "Número 1 es mayor y positivo";
+  }
+  else if ( num1 === 0 || num2 === 0 || num3 === 0 ){ 
+ return "Error";
+  }
+  else if (num3 > num1 &&  num3 > num2){
+  return num3 + 1;
+  }
+  else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -133,32 +153,61 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2) {
+    return false;
+  }
+  if (numero === 2) {
+    return true;
+  }
+   for (var i = 2 ; i < numero ; i++){ 
+    if (numero % i === 0){
+    return  false ;
+    } 
+  }
+  return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor === true ) {
+    return  "Soy verdadero"
+  }
+  return "Soy falso"
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí    
+  let matrizTablaDel6 = []
+  for(let i =  0 ; i < 11 ; i ++){
+  matrizTablaDel6.push( 6 * i)      
+  }
+  return  matrizTablaDel6;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero >=99 && numero <= 1000) {
+    return true;
+  }
+    return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+ let a = numero
+ let i = 0 
+ do {
+   i = i + 1;
+   a = a + 5;
+ } while (i < 8);
+  return a;
 }
 
 
