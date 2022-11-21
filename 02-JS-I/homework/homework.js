eb8274b0a4b1445e115da34ebfc83a6d6d2918c1
Josue David peñate ruiz <penateruizjosuedavid@gmail.com>
2022-11-21 11:36:29 -0500

    correccion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -186,7 +186,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinar = nombre + ' ' + apellido;
+  let combinar = nombre + ' ' + apellido;
     return combinar;
 }
 
